826f5b31ed5881b11f4fd9c873140f1838e64a52
kiyamakeita <kiyamakeita@gmail.com>
2015-06-01 01:49:40 +0900

    人物にvalid追加

--- app/models/ningen.rb
+++ app/models/ningen.rb
@@ -1,4 +1,8 @@
 class Ningen < ActiveRecord::Base
 	has_many :tasks
-	validates :name, presence: {message: "せめて名前ぐらい入れろや"}
+	validates :name, presence: {message: "せめて名前ぐらい入れろや"},
+			         length: {minimum: 2, message: "2文字以上の名前を入れてくれやい"}
+	validates :description, presence: {message: "せめて説明ぐらい入れろや"},
+			         length: {minimum: 6, message: "6文字以上の名前を入れてくれやい"}
+	validates :img_url, presence: {message: "画像のURLをいれてね"}
 end
