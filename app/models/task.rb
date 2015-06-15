b6e367835cc7b7cf0e771c5d8f25100d7c757bb1
kiyamakeita <kiyamakeita@gmail.com>
2015-06-15 20:55:22 +0900

    validは終わった気がする

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :ningen
+  validates :age, presence: {message: "年齢をいれてください"}
+  validates :event, presence: {message: "出来事をいれてください"},
+  					length: {minimum: 6, message: "6文字以上でお願いしますね"}
 end
